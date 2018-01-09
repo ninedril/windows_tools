a65537e7384acaee1e9ffe2b49ffd5ee7e35c44c
ninedril@t90chi <ninedril-t90chi@gmail.com>
2018-01-09 22:18:22 +0900

    Align argument var

--- taskschd/taskschd_python.bat
+++ taskschd/taskschd_python.bat
@@ -1,16 +1,16 @@
 : 第一引数はPythonスクリプトのファイル名、第二引数以降はスクリプトに渡す引数
-: @echo off
+@echo off
 for /F "tokens=* usebackq" %%i in (`where python`) do (
   set python_path=%%i
 )
 
-set script_name=%1
+set script_name=%~1
 set args=
 shift
 
 :loop
 if "%1"=="" goto final
-set args=%args% %1
+set args=%args% %~1
 shift
 goto loop
 :final
